70ab01e641219280c8b2fcca2a06f64c2dc58260
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-08-26 14:04:40 +0700

    Add Foto

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Parentbox">
+      <p>React JS</p>
+      <div className = "Foto">
+        <img src = {logo} alt = "logo" />
+      </div>
     </div>
   );
 }
